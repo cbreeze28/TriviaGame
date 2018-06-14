b73df609d8aedc06b3c3a1034b238ef102e4127b
Chris B <38058127+cbreeze28@users.noreply.github.com>
2018-06-14 15:08:46 -0400

    pulling question function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,15 @@
 var triviaQuestions = [{
         question: "what color is the grass?",
         answers: ["Blue", "Green", "Purple", "Red"],
-        answer: 
+        answer: 1
 },{
         question: "How many inches in a foot?",
         answers: ["12", "11", "16", "10",],
-        answer: 
+        answer: 0
 },{
         question: "How many days are in a week?",
         answers: ["5", "6", "7", "8",],
-        answer:   
+        answer:  2 
 }];
 
 $("#startButton").on("click", function(){
@@ -26,7 +26,19 @@ function triviaGame(){
     unansweredQuestions = 0;
     displayQuestion();
 }
-
+//pull questions from list
 function displayQuestion(){
+    $("correctAnswers").empty();
+    answered = true;
+    $("#questions").html("question" + (questions+1) + "/" + triviaQuestions.length);
+    $(".question").html("<h3>" + triviaQuestions[questions].question + "</h3>");
+    for (var i = 0; i < 4; i++){
+        var choices = $("<div>");
+        choices.text(triviaQuestions[questions].answers.length[i]);
+        choices.attr({"data-index": i});
+        choices.addClass("thisChoice");
+        $(".answers").append(choices);
+    }
+//Timer countdown
 
 }
