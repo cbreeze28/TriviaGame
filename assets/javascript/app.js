5d9ac168b62ce349884c57e5c630093159d305fb
Chris B <38058127+cbreeze28@users.noreply.github.com>
2018-06-14 14:47:04 -0400

    on click function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,32 @@
-$(document).ready(function() {
-    
+var triviaQuestions = [{
+        question: "?",
+        answers: ["", "", "", ""],
+        answer: 
+},{
+        question: "?",
+        answers: ["", "", "", "",],
+        answer: 
+},{
+        question: "?",
+        answers: ["", "", "", "",],
+        answer:   
+}];
+
+$("#startButton").on("click", function(){
+    $(this).hide();
+    triviaGame();
+});
+
+function triviaGame(){
+    $("#correctAnswers").empty();
+    $("#wrongAnswers").empty();
+    $("#unansweredQuestions").empty();
+    correctAnswer = 0;
+    wrongAnswer = 0;
+    unansweredQuestions = 0;
+    displayQuestion();
+}
+
+function displayQuestion(){
+
 }
