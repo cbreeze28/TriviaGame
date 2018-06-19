c91bcc9d7cd58d4b878bc3cdef3dc48e3fe32401
Chris B <38058127+cbreeze28@users.noreply.github.com>
2018-06-19 14:17:25 -0400

    JS functions should be working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,122 @@
-var triviaQuestions = [{
-        question: "what color is the grass?",
-        answers: ["Blue", "Green", "Purple", "Red"],
-        answer: 1
-},{
-        question: "How many inches in a foot?",
-        answers: ["12", "11", "16", "10",],
-        answer: 0
-},{
-        question: "How many days are in a week?",
-        answers: ["5", "6", "7", "8",],
-        answer:  2 
-}];
+var quizSection = $('#triviaQuestions');
+var timerStartValue = 30;
 
-$("#startButton").on("click", function(){
-    $(this).hide();
-    triviaGame();
+//start button
+$(document).on('click', '#restartButton', function(renew) {
+  triviaGame.reset();
 });
 
-function triviaGame(){
-    $("#correctAnswers").empty();
-    $("#wrongAnswers").empty();
-    $("#unansweredQuestions").empty();
-    correctAnswer = 0;
-    wrongAnswer = 0;
-    unansweredQuestions = 0;
-    displayQuestion();
-}
-//pull questions from list
-function displayQuestion(){
-    $("correctAnswers").empty();
-    answered = true;
-    $("#questions").html("question" + (questions+1) + "/" + triviaQuestions.length);
-    $(".question").html("<h3>" + triviaQuestions[questions].question + "</h3>");
-    for (var i = 0; i < 4; i++){
-        var choices = $("<div>");
-        choices.text(triviaQuestions[questions].answers.length[i]);
-        choices.attr({"data-index": i});
-        choices.addClass("thisChoice");
-        $(".answers").append(choices);
-    }
-//Timer countdown
+$(document).on('click', '.answer-button', function(renew) {
+  triviaGame.clicked(renew);
+});
 
-}
+$(document).on('click', '#startButton', function(renew) {
+  $('#timeElement').append('<h1>Time Remaining: <span id="counter-number">30</span> Seconds</h1>');
+  $("#instructionsTrivia").hide();
+  triviaGame.loadQuestion();
+});
+
+//questions
+var questions = [{
+  question: "what color is the grass?",
+  answers: ["Blue", "Red", "Green", "Yellow"],
+  correctAnswer: "Green",
+}];
+
+//Add mroe questions above in said format
+
+
+var triviaGame = {
+  questions:questions,
+  currentQuestion:0,
+  counter:timerStartValue,
+  correct:0,
+  incorrect:0,
+  countdown: function(){
+    triviaGame.counter--;
+    $('#counter-number').html(triviaGame.counter);
+
+    if (triviaGame.counter === 0){
+      console.log('TIME UP');
+      triviaGame.timeUp();
+    }
+  },
+  loadQuestion: function(){
+    timer = setInterval(triviaGame.countdown, 1000);
+    quizSection.html('<h2>' + questions[this.currentQuestion].question + '</h2>' );
+    for (var i = 0; i<questions[this.currentQuestion].answers.length; i++){
+      quizSection.append('<button class="answer-button" id="button"' + 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i]+ '</button>');
+    }
+  },
+  nextQuestion: function(){
+    triviaGame.counter = timerStartValue;
+    $('#counter-number').html(triviaGame.counter);
+    triviaGame.currentQuestion++;
+    triviaGame.loadQuestion();
+  },
+  timeUp: function (){
+    clearInterval(timer);
+    $('#counter-number').html(triviaGame.counter);
+
+    quizSection.html('<h2>Out of Time!</h2>');
+    quizSection.append('<h3>The Correct Answer was: ' + questions[this.currentQuestion].correctAnswer);
+    quizSection.append('<img src="' + questions[this.currentQuestion].image + '" />');
+
+    if (triviaGame.currentQuestion === questions.length - 1){
+      setTimeout(triviaGame.results, 3 * 1000);
+    } else {
+      setTimeout(triviaGame.nextQuestion, 3 * 1000);
+    }
+  },
+  results: function() {
+    clearInterval(timer);
+
+    quizSection.html('<h2>All done, heres how you did!</h2>');
+    $('#counter-number').html(triviaGame.counter);
+    quizSection.append('<h3>Correct Answers: ' + triviaGame.correct + '</h3>');
+    quizSection.append('<h3>Incorrect Answers: ' + triviaGame.incorrect + '</h3>');
+    quizSection.append('<h3>Unanswered: ' + (questions.length - (triviaGame.incorrect + triviaGame.correct)) + '</h3>');
+    quizSection.append('<br><button id="start-over">Start Over?</button>');
+  },
+  clicked: function(renew) {
+    clearInterval(timer);
+
+    if ($(renew.target).data("name") === questions[this.currentQuestion].correctAnswer){
+      this.answeredCorrectly();
+    } else {
+      this.answeredIncorrectly();
+    }
+  },
+  answeredIncorrectly: function() {
+    triviaGame.incorrect++;
+    clearInterval(timer);
+    quizSection.html('<h2>Nope!</h2>');
+    quizSection.append('<h3>The Correct Answer was: ' + questions[triviaGame.currentQuestion].correctAnswer + '</h3>');
+    quizSection.append('<img src="' + questions[triviaGame.currentQuestion].image + '" />');
+
+    if (triviaGame.currentQuestion === questions.length - 1){
+      setTimeout(triviaGame.results, 3 * 1000);
+    } else {
+      setTimeout(triviaGame.nextQuestion, 3 * 1000);
+    }
+  },
+  answeredCorrectly: function(){
+    clearInterval(timer);
+    triviaGame.correct++;
+    quizSection.html('<h2>Correct!</h2>');
+    quizSection.append('<img src="' + questions[triviaGame.currentQuestion].image + '" />');
+
+    if (triviaGame.currentQuestion === questions.length - 1){
+      setTimeout(triviaGame.results, 3 * 1000);
+    } else {
+      setTimeout(triviaGame.nextQuestion, 3 * 1000);
+    }
+  },
+  reset: function(){
+    this.currentQuestion = 0;
+    this.counter = timerStartValue;
+    this.correct = 0;
+    this.incorrect = 0;
+    this.loadQuestion();
+  }
+};
